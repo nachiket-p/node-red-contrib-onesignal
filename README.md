2cc4224dade98220fe3837664deaea3e975b2c51
Ronit Kadwane <ronit@jumpbyte.com>
2017-05-05 12:06:28 +0530

    1. platform specific notification 2. segment specific notification

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # node-red-contrib-onesignal
-Node-RED node for push notifications. 
+ <h3>
+ Node-RED node for push notifications.
+    </h3>
+    <p>
+        A node which is capable enough to send push notification on following platforms.
+        <ul>
+            <li>
+                <b>WEB</b>
+            </li>
+            <li>
+                <b>ANDROID</b>
+            </li>
+            <li>
+                <b>iOS</b>
+            </li>
+        </ul>
+    </p>
+    <h2>Node Configuration</h2>
+    <div>
+        <b>OneSignal App Id</b>
+        <div> Configure OneSignal app id and Rest api key.</div></br>
+    </div>
+    <div>
+        <b>Title</b>
+        <div> It containes title of push notification. </br> You can add title from <code>msg.payload.title</code> or can type manually.</div></br>
+    </div>
+    <div>
+        <b>Message</b>
+        <div> It containes body content of message. </br> You can add message content from <code>msg.payload.content</code> or can type manually.</div></br>
+    </div>
+    <div>
+        <b>Platform</b>
+        <div> To only send to specific platform, you may check only that platform. </br> select at least one platform to send push notification.</div></br>
+    </div>
+    <div>
+        <b>AddSegment</b>
+        <div> you simply specify which segments you want to send to.</div>
+    </div>Ï
--- onesignal/onesignal.js
+++ onesignal/onesignal.js
@@ -1,63 +1,84 @@
 var https = require('https');
 
-module.exports = function(RED) {
-    function OneSignalNode(config) {
-        console.log("CONFIG: ", config)
-        RED.nodes.createNode(this,config);
-        var oneSignalConfig = RED.nodes.getNode(config.config);
+module.exports = function (RED) {
+  function OneSignalNode(config) {
+    console.log("CONFIG: ", config)
+    RED.nodes.createNode(this, config);
+    var oneSignalConfig = RED.nodes.getNode(config.config);
+    
+    var node = this;
+    var headers = {
+      "Content-Type": "application/json; charset=utf-8",
+      "Authorization": "Basic " + oneSignalConfig.restApiKey
+    };
 
-        var node = this;
-        var headers = {
-          "Content-Type": "application/json; charset=utf-8",
-          "Authorization": "Basic " + oneSignalConfig.restApiKey
-        };
-        
-        var options = {
-          host: "onesignal.com",
-          port: 443,
-          path: "/api/v1/notifications",
-          method: "POST",
-          headers: headers
-        };
+    var options = {
+      host: "onesignal.com",
+      port: 443,
+      path: "/api/v1/notifications",
+      method: "POST",
+      headers: headers
+    };
+
+    this.on('input', function (msg) {
+      console.log('INSIDE input');
+      console.log('input msg:-', msg);
       
-        this.on('input', function(msg) {
-          //msg.payload = msg.payload.toLowerCase();
-          //node.send(msg);
-          var req = https.request(options, function(res) {  
-            res.on('data', function(data) {
-              var response = JSON.parse(data);
-              if(response.errors) {
-                response.errors.forEach(function(err){node.error(err);})
-                node.status({fill:"red",shape:"ring",text:"OneSignal ERROR"});
-              } else {
-                node.log("Notification SENT SUCCESSFULLY");
-                node.status({fill:"green",shape:"ring",text:"SENT!"});
-              }
-              //orignalRes.send(JSON.stringify(response))
-            });
-
-            req.on('error', function(e) {
-              console.log("ERROR:", e);
-              node.error(e)
-              node.status({fill:"red",shape:"ring",text:"OneSignal ERROR"});
-            });
-          });
-          
-          
-          
-          var text = msg.payload.message || config.defaultMessage;
-          var title = msg.payload.title || config.defaultTitle;
-
-          var messageJson = { 
-            app_id: oneSignalConfig.appId,
-            contents: {"en": text},
-            headings: {"en": title},
-            included_segments: ["All"]
-          };
-
-          req.write(JSON.stringify(messageJson));
-          req.end();
+      var req = https.request(options, function (res) {
+        res.on('data', function (data) {
+          var response = JSON.parse(data);
+          if (response.errors) {
+            response.errors.forEach(function (err) {
+              console.log("ONESIGNAL Responce Error:", err);
+              node.error("ONESIGNAL Responce Error", err);
+            })
+            node.status({ fill: "red", shape: "ring", text: "OneSignal ERROR" });
+          } else {
+            node.log("Notification SENT SUCCESSFULLY");
+            node.status({ fill: "green", shape: "ring", text: "SENT!" });
+          }
+          //orignalRes.send(JSON.stringify(response))
         });
-    }
-    RED.nodes.registerType("onesignal",OneSignalNode);
+
+        req.on('error', function (e) {
+          console.log("ONE SIGNAL ERROR:", e);
+          node.error("ONE SIGNAL ERROR", e);
+          node.status({ fill: "red", shape: "ring", text: "OneSignal ERROR" });
+        });
+      });
+  
+      var title = msg.payload.title || config.title;
+      var text =  msg.payload.content || config.message;
+      var web = config.web;
+      var android = config.android;
+      var ios = config.ios;
+      var segment = config.segments;
+
+      var messageJson = {
+        app_id: oneSignalConfig.appId,
+        headings: { "en": title },
+        contents: { "en": text },
+        included_segments: segment
+      };
+
+      if (web) {
+        messageJson.isAnyWeb = true;
+      }
+      if (android) {
+        messageJson.isAndroid = true;
+      }
+      if (ios) {
+        messageJson.isIos = true;
+      }
+      if (!web && !android && !ios) {
+        console.log('You didn`t select any platform');
+        req.end();
+      } else {
+        console.log('OneSignal Message:- ', messageJson);
+        req.write(JSON.stringify(messageJson));
+        req.end();
+      }
+    });
+  }
+  RED.nodes.registerType("onesignal", OneSignalNode);
 }
